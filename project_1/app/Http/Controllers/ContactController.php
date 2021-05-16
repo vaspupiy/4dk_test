f3d8c40b4b017c2a354b185d0d9cbff9216d1f18
Vasiliy <vaspupiy@mail.ru>
2021-05-16 23:37:26 +0300

    lesson4

---
+++ project_1/app/Http/Controllers/ContactController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Contact;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+
+class ContactController extends Controller
+{
+    public function submit(ContactRequest $req)
+    {
+        $contact = new Contact();
+        $contact->name = $req->input('name');
+        $contact->email = $req->input('email');
+        $contact->subject = $req->input('subject');
+        $contact->message = $req->input('message');
+
+        $contact->save();
+
+        return redirect()->route('home')->with('success', 'сообщение было добавлено');
+    }
+}
--- project_1/resources/views/layouts/app.blade.php
+++ project_1/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,16 +9,21 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     @include('inc.header')
 
-    @if(Request::is('/'))
+    @if (Request::is('/'))
         @include('inc.hero')
     @endif
+
+
+
     <div class="container mt-5">
+        @include('inc.messages')
         <div class="row">
             <div class="col-8">
-                @yield('content')   
+                @yield('content')
             </div>
             <div class="col-4">
                 @include('inc.aside')
@@ -27,4 +33,5 @@
 
     @include('inc.footer')
 </body>
+
 </html>
